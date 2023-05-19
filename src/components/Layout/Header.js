76368a0117975f7dd374a222da5881cdc03cac51
Rishal <Rishal.mhd009@gmail.com>
2023-05-19 13:51:17 +0530

    added the image and header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals Go</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src="https://images.unsplash.com/photo-1576867757603-05b134ebc379?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+          alt="food"
+        />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
