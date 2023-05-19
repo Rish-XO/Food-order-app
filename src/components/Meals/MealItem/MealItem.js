37abb2f728da2df1a5e7cb45ce81eaa89b8ac748
Rishal <Rishal.mhd009@gmail.com>
2023-05-19 17:17:07 +0530

    components UI done, next to login

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,9 @@
 import React from "react";
-import classes from './MealItem.module.css'
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 function MealItem(props) {
-    const price = `₹${props.price.toFixed(2)}`;
+  const price = `₹${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +12,7 @@ function MealItem(props) {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   );
